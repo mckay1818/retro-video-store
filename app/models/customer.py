5c0b8e1e3fe574bdc2e57bd2b7a2d9eace34434d
mckay <mckaybwarren@gmail.com>
2023-01-03 17:20:09 -0500

    register customer and video models

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,8 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String)
+    registered_at = db.Column(db.DateTime)
+    videos_checked_out_count = db.Column(db.Integer)
