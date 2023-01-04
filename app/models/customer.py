6a9861653c109e139a57d0703df96d537ed364a1
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-01-04 13:14:00 -0700

    feat: Create create_one_customer end point

--- app/models/customer.py
+++ app/models/customer.py
@@ -3,6 +3,7 @@ from app import db
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    registered_at = db.Column(db.DateTime)
     postal_code = db.Column(db.String)
     phone = db.Column(db.String)
     
--- app/routes.py
+++ app/routes.py
@@ -13,8 +13,23 @@ def read_all_customers():
     for customer in customers:
         customers_response.append({
             "id": customer.id,
-            "name": customer.id,
-            "postal_code": customer.id,
+            "name": customer.name,
+            "postal_code": customer.postal_code,
             "phone": customer.phone
         })
     return jsonify(customers_response)
+
+@customers_bp.route("", methods = ["POST"])
+def create_one_customer():
+    request_body = request.get_json()
+    new_customer = Customer(
+        name = request_body["name"],
+        registered_at = request_body["registered_at"],
+        postal_code = request_body["postal_code"],
+        phone = request_body["phone"]
+    )
+
+    db.session.add(new_customer)
+    db.session.commit()
+
+    return make_response({"id": {new_customer.id}},201)
