803b531295364b87bd8cad602f406d03c494b778
mckay <mckaybwarren@gmail.com>
2023-01-03 19:43:10 -0500

    create endpoints for reading all or 1 videos, creating new video

--- app/models/video.py
+++ app/models/video.py
@@ -5,3 +5,16 @@ class Video(db.Model):
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime)
     total_inventory = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "total_inventory": self.total_inventory
+        }
+
+    @classmethod
+    def from_dict(cls, video_data):
+        new_video = Video(title=video_data["title"], release_date=video_data["release_date"], total_inventory=video_data["total_inventory"])
+        return new_video
--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,41 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+
+def validate_video_id(video_id):
+    try:
+        video_id = int(video_id)
+    except:
+        abort(make_response(jsonify({"message": "Video ID {video_id} invalid"}), 400))
+
+    video = Video.query.get(video_id)
+
+    if not video:
+        abort(make_response(jsonify({"message": "Video ID #{video_id} not found"}), 404))
+
+@videos_bp.route("", methods=["POST"])
+def create_video():
+    request_body = request.get_json()
+    new_video = Video.from_dict(request_body)
+
+    db.session.add(new_video)
+    db.session.commit()
+
+    return make_response(jsonify(f"Video {new_video.title} successfully created."), 201)
+
+@videos_bp.route("", methods=["GET"])
+def get_all_videos():
+    #add logic for filtering by query params
+    videos = Video.query.all()
+    videos_response = []
+    for video in videos:
+        videos_response.append(video.to_dict())
+    return jsonify(videos_response)
+
+@videos_bp.route("/<video_id>", methods=["GET"])
+def get_one_video(video_id):
+    video = validate_video_id(video_id)
+    return video.to_dict()
+
+# @videos_bp.route("") 
+#     pass
