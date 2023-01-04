b77c3afd86f0325b35a355048604134fe6b855f3
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-01-04 10:54:29 -0700

    feat: Create read_all_customers end point

--- app/models/customer.py
+++ app/models/customer.py
@@ -5,5 +5,5 @@ class Customer(db.Model):
     name = db.Column(db.String)
     postal_code = db.Column(db.String)
     phone = db.Column(db.String)
-    registered_at = db.Column(db.DateTime)
-    videos_checked_out_count = db.Column(db.Integer)
+    
+
--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,16 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+
+@customers_bp.route("",methods = ["GET"])
+def read_all_customers():
+    customers = Customer.query.all()
+    customers_response = []
+    for customer in customers:
+        customers_response.append({
+            "id": customer.id,
+            "name": customer.id,
+            "postal_code": customer.id,
+            "phone": customer.phone
+        })
+    return jsonify(customers_response)
