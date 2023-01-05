aeaafd7c17503e36b472f1d09ddc8eff5698317b
mckay <mckaybwarren@gmail.com>
2023-01-05 15:47:45 -0500

    implement generalized validation fns into Video routes

--- app/routes.py
+++ app/routes.py
@@ -3,20 +3,11 @@ from app.models.customer import Customer
 from app.models.video import Video
 from flask import Blueprint, jsonify, abort, make_response, request
 
+# Define Blueprints
 videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 
-def validate_video_id(video_id):
-    try:
-        video_id = int(video_id)
-    except:
-        abort(make_response(jsonify({"message": f"Video {video_id} is invalid"}), 400))
-
-    video = Video.query.get(video_id)
-    if not video:
-        abort(make_response(jsonify({"message": f"Video {video_id} was not found"}), 404))
-    return video
-
+# Validation Helper Fns
 def validate_model_data_and_create_obj(cls, model_data):
     try:
         new_obj = cls.from_dict(model_data)
@@ -25,6 +16,28 @@ def validate_model_data_and_create_obj(cls, model_data):
             abort(make_response(jsonify({"details": f"Request body must include {key}."}), 400))
     return new_obj
 
+def validate_model_id(cls, model_id):
+    
+    try:
+        model_id = int(model_id)
+    except:
+        # handling invalid id type
+        abort(make_response({"message":f"{cls.__name__} {model_id} was invalid"}, 400))
+
+    # return obj data if id in db
+    model = cls.query.get(model_id)
+
+    # handle nonexistant id
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} was not found"}, 404))
+    return model
+
+
+##################
+# CUSTOMER ROUTES #
+##################
+
+# GET
 @customers_bp.route("",methods = ["GET"])
 def read_all_customers():
     customers = Customer.query.all()
@@ -33,6 +46,12 @@ def read_all_customers():
         customers_response.append(customer.to_dict())
     return jsonify(customers_response)
 
+@customers_bp.route("/<customer_id>", methods=["GET"])
+def get_one_customer(customer_id):
+    customer = validate_model_id(Customer, customer_id)
+    return customer.to_dict()
+
+# POST
 @customers_bp.route("", methods = ["POST"])
 def create_one_customer():
     request_body = request.get_json()
@@ -41,7 +60,43 @@ def create_one_customer():
     db.session.add(new_customer)
     db.session.commit()
 
-    return new_customer.to_dict(), 201 
+    return new_customer.to_dict(), 201
+
+# PUT
+@customers_bp.route("/<customer_id>", methods=["PUT"]) 
+def update_one_customer(customer_id):
+    customer = validate_model_id(Customer, customer_id)
+    request_body = request.get_json()
+    try:
+        customer.name = request_body["name"]
+        customer.phone = request_body["phone"]
+        customer.postal_code = request_body["postal_code"]
+
+    # #TODO: refactor this out of Video model, into sep fn in routes
+    except KeyError as e:
+        key = str(e).strip("\'")
+        abort(make_response(jsonify({"details": f"Request body must include {key}."}), 400))
+    
+    db.session.commit()
+    return {
+        "id" : customer.id,
+        "name": customer.name,
+        "postal_code": customer.postal_code,
+        "phone": customer.phone
+    }
+
+# DELETE
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_one_customer(customer_id):
+    customer = validate_model_id(Customer,customer_id)
+    db.session.delete(customer)
+    db.session.commit()
+    return customer.to_dict()
+
+
+##################
+##  VIDEO ROUTES  ##
+##################
 
 @videos_bp.route("", methods=["POST"])
 def create_video():
@@ -64,19 +119,19 @@ def get_all_videos():
 
 @videos_bp.route("/<video_id>", methods=["GET"])
 def get_one_video(video_id):
-    video = validate_video_id(video_id)
+    video = validate_model_id(Video, video_id)
     return video.to_dict()
 
 @videos_bp.route("/<video_id>", methods=["PUT"]) 
 def update_one_video(video_id):
-    video = validate_video_id(video_id)
+    video = validate_model_id(Video, video_id)
     request_body = request.get_json()
     try:
         video.title = request_body["title"]
         video.release_date = request_body["release_date"]
         video.total_inventory = request_body["total_inventory"]
 
-    # #TODO: refactor this out of Video model, into sep fn in routes
+    #TODO: refactor this out of Video model, into sep fn in routes
     except KeyError as e:
         key = str(e).strip("\'")
         abort(make_response(jsonify({"details": f"Request body must include {key}."}), 400))
@@ -86,7 +141,7 @@ def update_one_video(video_id):
 
 @videos_bp.route("/<video_id>", methods=["DELETE"])
 def delete_one_video(video_id):
-    video = validate_video_id(video_id)
+    video = validate_model_id(Video, video_id)
 
     db.session.delete(video)
     db.session.commit()
